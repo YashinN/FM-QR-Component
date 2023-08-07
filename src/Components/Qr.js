9a95200d94e10e428a174970d4443af25c5946b8
Yashin <yashinnaran@gmail.com>
2023-08-07 21:35:43 +0200

    QR compononet completed

--- src/Components/Qr.js
+++ src/Components/Qr.js
@@ -3,6 +3,15 @@ const Qr = () => {
     <div className="container">
       <div className="qr-card">
         <img src="/image-qr-code.png" alt="qr code" className="qr-img" />
+
+        <h1 className="title">
+          Improve your front-end <span> skills by building projects</span>
+        </h1>
+
+        <p className="text">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
       </div>
     </div>
   );
